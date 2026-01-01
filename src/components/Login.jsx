fb6af15efc59b8f2cc3a5f23338bcccbaf9d331e
Usama El-Sharkawi <usamasharkawi@gmail.com>
2026-01-01 11:47:00 +0200

    Getting user input via Refs

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,21 +1,17 @@
-import { useState } from "react";
+import { useRef } from "react";
 
 export default function Login() {
-  const [enteredValues, setEnteredValues] = useState({
-    email: "",
-    password: "",
-  });
+  const email = useRef();
+  const password = useRef();
 
   function handleSubmit(event) {
     event.preventDefault();
-    console.log(enteredValues);
+
+    const enteredEmail = email.current.value;
+    const enteredPassword = password.current.value;
+    console.log(enteredEmail,enteredPassword)
   }
-  function handleChangeValue(identifier, value) {
-    setEnteredValues((prevValues) => ({
-      ...prevValues,
-      [identifier]: value,
-    }));
-  }
+
   return (
     <form onSubmit={handleSubmit}>
       <h2>Login</h2>
@@ -23,28 +19,12 @@ export default function Login() {
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input
-            id="email"
-            type="email"
-            name="email"
-            onChange={(event) => {
-              handleChangeValue("email", event.target.value);
-            }}
-            value={enteredValues.email}
-          />
+          <input id="email" type="email" name="email" ref={email} />
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input
-            id="password"
-            type="password"
-            name="password"
-            onChange={(event) => {
-              handleChangeValue("password", event.target.value);
-            }}
-            value={enteredValues.password}
-          />
+          <input id="password" type="password" name="password" ref={password} />
         </div>
       </div>
 
